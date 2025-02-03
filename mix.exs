35c42ca020e8683b219df74a790ba1e6470153ac
Mattias <mattiasmeiner@gmail.com>
2025-02-03 13:02:07 +0100

    add credo

--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule Yugo.MixProject do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 
