30b05534fc73969206cd76826b4f44f92395df75
Mattias <mattiasmeiner@gmail.com>
2025-02-04 08:46:45 +0100

    increase elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Yugo.MixProject do
     [
       app: :yugo,
       version: "1.0.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Yugo is an easy and high-level IMAP client library.",
